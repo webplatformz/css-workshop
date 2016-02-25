17343e0afd03d04793dfc638304ae02ed03ea68e
r3dDoX <patrick@walther-online.ch>
2016-02-25 14:54:37 +0100

    allow basic selecting

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('app running');
+document.addEventListener("DOMContentLoaded", function() {
+    var playerElements = document.querySelectorAll('div.player');
+    playerElements.forEach(function(player) {
+        player.addEventListener('click', function() {
+            playerElements.forEach(function(element) {
+                if (element === player && !element.classList.contains('selected')) {
+                    element.classList.add('selected')
+                } else {
+                    element.classList.remove('selected')
+                }
+            });
+        });
+    });
+});
